bfeb9df39f44fac1f3e80c629589b89ac1ee8cb8
Sparton-dou <nicolas.nowinski@gmail.com>
2025-10-21 21:08:11 +0200

    docker delete

--- containers/imaginary/healthcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nc -z 127.0.0.1 "$PORT" || exit 1
--- containers/imaginary/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Imaginary has started"
-if [ -z "$IMAGINARY_SECRET" ]; then
-    imaginary -return-size -max-allowed-resolution 222.2 "$@"
-else
-    imaginary -return-size -max-allowed-resolution 222.2 -key "$IMAGINARY_SECRET" "$@"
-fi
